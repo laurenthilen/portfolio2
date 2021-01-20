c15b81a51d15c8672cb58875ecfd0c61900ce42d
Lauren Emick <laurenemick6@gmail.com>
2021-01-19 16:24:25 -0800

    adds project data under data folder and creates project apps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import projects from "./data/projects";
 import "./App.css";
 
 function App() {
@@ -6,9 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Lauren Emick</h1>
-        <p>Body text</p>
-
       </header>
+
+      <div className="projects-container">
+        {
+          projects.map(project => {
+            return (
+              <div className="project">
+                <img className="project-img" src={project.imgURL} alt={project.name} />
+                <p>{project.name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
 
       <div className="blurr-box-container">
         <footer className="blurr-box">
