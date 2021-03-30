50c8b22b8acf93bbe79b10e2dacd65208ab88c63
Lauren Emick <laurenemick6@gmail.com>
2021-03-30 16:30:20 -0700

    new images for project app icons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => {
                     projects.map(project => (
                         <Link className="project-link" key={project.id} to={`/projects/${project.id}`}>
                             <div className="project-icon">
-                                <img className="projects-img" src={project.imgURL} alt={project.name} />
+                                <img className="projects-img" style={{backgroundColor: project.bgcolor}} src={project.icon} alt={project.name} />
                                 <p className="projects-name">{project.name}</p>
                             </div>
                         </Link>
